abef9ce225e8dc0de6b2800d303fc771cdbfc64c
justingreco <justin.greco@raleighnc.gov>
2016-09-20 23:39:54 -0400

    make email address not case sensitive

--- app/src/LoginController.js
+++ app/src/LoginController.js
@@ -7,7 +7,7 @@
 function LoginController(UsersDataService, $mdSidenav, $http, $filter, $scope, $timeout, $location, $state) {
   var self = this;
   self.login = function (credentials) {
-    credentials.email.toLowerCase();
+    credentials.email = credentials.email.toLowerCase();
     $http.post('http://mapstest.raleighnc.gov/parks-form-api/login', credentials).then(function (result) {
       if (result.data.success)
         $state.go('form', {user: result.data.user});
